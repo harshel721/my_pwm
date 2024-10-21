ca50a47e563311d34c8ed14d73830a64f90c2e3f
harshelm <harshelm@vayavyalabs.com>
2024-10-21 13:18:24 +0530

    changed a few things in UART

--- LedBlinking.c
+++ LedBlinking.c
@@ -1,4 +1,5 @@
 #include "LedBlinking.h"
+#include "Uart.h"
 
 extern const Mcu_ConfigType McuConfigSet;
 extern const Port_ConfigType PortConfigSet;
@@ -6,54 +7,54 @@ extern Mcu_GlobalType McuGlobal;
 extern const Pwm_ConfigType PwmConfigSet;
 
 void delay(int count) {
-  for (int i = 0; i < (count * 1000); i++);
+  for (int i = 0; i < (count * 1000); i++)
+    ;
 }
 
-
-
-//int main() {
-//  boolean mcuStatus;
-//  Mcu_PllStatusType status;
-//  Mcu_Init(&McuConfigSet);
-//  Mcu_InitClock(McuConf_McuClockSetting_Id_0);
+// int main() {
+//   boolean mcuStatus;
+//   Mcu_PllStatusType status;
+//   Mcu_Init(&McuConfigSet);
+//   Mcu_InitClock(McuConf_McuClockSetting_Id_0);
 
 //  if (status == MCU_PLL_LOCKED) {
 //    Mcu_DistributePllClock();
 //  }
-//	
+//
 //	Port_Init(&PortConfigSet);
-//	
+//
 //	Pwm_Init(&PwmConfigSet);
 //}
 
 // System initialization function
 void init_system(void) {
   Mcu_PllStatusType status;
-  
+
   // Initialize MCU
   Mcu_Init(&McuConfigSet);
   Mcu_InitClock(McuConf_McuClockSetting_Id_0);
-  
+
   // Check if PLL is locked
   if (status == MCU_PLL_LOCKED) {
     Mcu_DistributePllClock();
   }
-	
-	uart_init1(DEBUG_UART, 84, UART_BAUD_115200, UART_WL_8BIT, UART_STP_BIT_1, UART_PARITY_EVEN, FALSE);
-	
+
+  uart_init1(DEBUG_UART, PCLK, UART_BAUD_9600, UART_WL_8BIT, UART_STP_BIT_1,
+             UART_PARITY_EVEN, FALSE);
+
   // Initialize Port
   Port_Init(&PortConfigSet);
-	uart_printf("Mcu and Port Initilization done\n\r");
+  uart_printf("Mcu and Port Initilization done\n\r");
 
   // Initialize PWM
   Pwm_Init(&PwmConfigSet);
-	uart_printf("Pwm Initilization done\n\r");
+  uart_printf("Pwm Initilization done\n\r");
 }
 
 void led_fade(void) {
   int duty_cycle;
   const int max_brightness = 999; // Maximum duty cycle value
-  const int step = 1;              // Step size for fading
+  const int step = 1;             // Step size for fading
 
   // Fade-in effect
   for (duty_cycle = 0; duty_cycle <= max_brightness; duty_cycle += step) {
@@ -71,7 +72,7 @@ void led_fade(void) {
 int main() {
   // Initialize the system components
   init_system();
-  
+
   // Continuously fade the LED in and out
   while (1) {
     led_fade();
@@ -79,4 +80,3 @@ int main() {
 
   return 0;
 }
-
--- Uart.c
+++ Uart.c
@@ -1,85 +1,105 @@
- #include "uart.h"
+#include "Uart.h"
 #include "McuRegs.h"
 #include "PortRegs.h"
 
 extern Mcu_RCC *RCC;
 extern PortRegsType *PortRegs[9];
-static UartRegsType *UartRegs[] = {(UartRegsType*)0x40011000,(UartRegsType*)0x40004400,(UartRegsType*)0x40004800,(UartRegsType*)0x40004C00,(UartRegsType*)0x40005000, (UartRegsType*)0x40011400};
+static UartRegsType *UartRegs[] = {
+    (UartRegsType *)0x40011000, (UartRegsType *)0x40004400,
+    (UartRegsType *)0x40004800, (UartRegsType *)0x40004C00,
+    (UartRegsType *)0x40005000, (UartRegsType *)0x40011400};
 
-void uart_init1(uint8 idx, uint32 pclk_mhz, UartBaudType BaudRate, UartWordLenType Wls, UartStopBitType StpBit, UartParityType Parity, boolean ParityCheck)
-{
-    uint32 CR1Val = 0x00;
-    uint32 CR2Val = 0x00;
-    uint16 DivVal;
-    float DivValFraction;
-	UartRegs[1]->BRR = 0x00000000;
-    switch(BaudRate) {
-        case UART_BAUD_4800: UartRegs[1]->BRR = 0x222E; break;
-        case UART_BAUD_7200: UartRegs[1]->BRR = 0x16C9; break;
-        case UART_BAUD_9600: UartRegs[1]->BRR = 0x1117; break;
-        case UART_BAUD_14400: UartRegs[1]->BRR = 0xB65; break;
-        case UART_BAUD_19200: UartRegs[1]->BRR = 0x889; break;
-        case UART_BAUD_57600: UartRegs[1]->BRR =  0x2DA; break;
-        case UART_BAUD_115200: UartRegs[1]->BRR = 0x16D; break;
-        default: UartRegs[1]->BRR = 0x1117;
+void uart_init1(uint8 idx, uint32 pclk_mhz, UartBaudType BaudRate,
+                UartWordLenType Wls, UartStopBitType StpBit,
+                UartParityType Parity, boolean ParityCheck) {
+  uint32 CR1Val = 0x00;
+  uint32 CR2Val = 0x00;
+  uint16 DivVal;
+  float DivValFraction;
+  UartRegs[1]->BRR = 0x00000000;
+  switch (BaudRate) {
+  case UART_BAUD_4800:
+    UartRegs[1]->BRR = 0x222E;
+    break;
+  case UART_BAUD_7200:
+    UartRegs[1]->BRR = 0x16C9;
+    break;
+  case UART_BAUD_9600:
+    UartRegs[1]->BRR = 0x1117;
+    break;
+  case UART_BAUD_14400:
+    UartRegs[1]->BRR = 0xB65;
+    break;
+  case UART_BAUD_19200:
+    UartRegs[1]->BRR = 0x889;
+    break;
+  case UART_BAUD_57600:
+    UartRegs[1]->BRR = 0x2DA;
+    break;
+  case UART_BAUD_115200:
+    UartRegs[1]->BRR = 0x16D;
+    break;
+  default:
+    UartRegs[1]->BRR = 0x1117;
+  }
+
+  // Configure word length
+  if (Wls == UART_WL_8BIT) {
+    CR1Val &= ~(1 << 12);
+  } else if (Wls == UART_WL_9BIT) {
+    CR1Val |= (1 << 12);
+  }
+
+  // Configure stop bits
+  switch (StpBit) {
+  case UART_STP_BIT_1:
+    CR2Val &= ~(0x03 << 12);
+    break;
+  case UART_STP_BIT_05:
+    CR2Val |= (0x01 << 12);
+    break;
+  case UART_STP_BIT_2:
+    CR2Val |= (0x02 << 12);
+    break;
+  case UART_STP_BIT_15:
+    CR2Val |= (0x03 << 12);
+    break;
+  default:
+    CR2Val &= ~(0x03 << 12);
+  }
+
+  // Configure parity
+  if (ParityCheck) {
+    CR1Val |= (1 << 10);
+    if (Parity == UART_PARITY_ODD) {
+      CR1Val |= (1 << 9);
+    } else if (Parity == UART_PARITY_EVEN) {
+      CR1Val &= ~(1 << 9);
     }
+  } else {
+    CR1Val &= ~(1 << 10);
+  }
 
-    // Configure word length
-    if (Wls == UART_WL_8BIT) {
-        CR1Val &= ~(1 << 12);
-    } else if (Wls == UART_WL_9BIT) {
-        CR1Val |= (1 << 12);
-    }
+  UartRegs[idx]->CR1 = CR1Val;
+  UartRegs[idx]->CR2 = CR2Val;
 
-    // Configure stop bits
-    switch (StpBit) {
-        case UART_STP_BIT_1:
-            CR2Val &= ~(0x03 << 12);
-            break;
-        case UART_STP_BIT_05:
-            CR2Val |= (0x01 << 12);
-            break;
-        case UART_STP_BIT_2:
-            CR2Val |= (0x02 << 12);
-            break;
-        case UART_STP_BIT_15:
-            CR2Val |= (0x03 << 12);
-            break;
-		default: CR2Val &= ~(0x03 << 12);
-    }
-
-    // Configure parity
-    if (ParityCheck) {
-        CR1Val |= (1 << 10);
-        if (Parity == UART_PARITY_ODD) {
-            CR1Val |= (1 << 9);
-        } else if (Parity == UART_PARITY_EVEN) {
-            CR1Val &= ~(1 << 9);
-        }
-    } else {
-        CR1Val &= ~(1 << 10);
-    }
-
-    UartRegs[idx]->CR1 = CR1Val;
-    UartRegs[idx]->CR2 = CR2Val;
-
-    // Enable UART and transmitter
-    UartRegs[idx]->CR1 |= (1 << 13); // USART Enable
-    UartRegs[idx]->CR1 |= (1 << 3);  // TX Enable
+  // Enable UART and transmitter
+  UartRegs[idx]->CR1 |= (1 << 13); // USART Enable
+  UartRegs[idx]->CR1 |= (1 << 3);  // TX Enable
 }
 
-void USART2_write(char ch)
-{
-	UartRegs[1]->DR &= ~(0xff);
-	UartRegs[1]->DR |= (ch);
-	while(!(UartRegs[1]->SR & (1<<7)));
+void USART2_write(char ch) {
+  UartRegs[1]->DR &= ~(0xff);
+  UartRegs[1]->DR |= (ch);
+  while (!(UartRegs[1]->SR & (1 << 7)))
+    ;
 }
 
-void uart_printf(char *str)
-{
-	while(*str) {
-		int i;
-		USART2_write(*(str++));
-		for(i=0; i<1000; i++); // some delay
-	}
+void uart_printf(char *str) {
+  while (*str) {
+    int i;
+    USART2_write(*(str++));
+    for (i = 0; i < 1000; i++)
+      ; // some delay
+  }
 }
